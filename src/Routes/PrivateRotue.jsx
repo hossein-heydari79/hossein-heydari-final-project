8edfcd730e23ba181cd66267a10fa8487247904b
hossein-heydari <hosseinheydari790228@gmail.com>
2021-08-21 17:09:03 +0430

    add private routes

---
+++ src/Routes/PrivateRotue.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { Route, Redirect } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+
+const PrivateRotue = ({ path, exact, loginRequired, Component }) => {
+    const isLogin = useSelector(state => state.User.isLogin)
+    return (
+        <Route
+            path={path}
+            exact={exact}
+            render={({ location }) =>
+                (!loginRequired) || (loginRequired && isLogin) ? (
+                    <Component />
+                ) : (
+                    <Redirect
+                        to={{
+                            pathname: "/login",
+                            state: { from: location }
+                        }}
+                    />
+                )
+            }
+        />
+    )
+}
+export default PrivateRotue;
