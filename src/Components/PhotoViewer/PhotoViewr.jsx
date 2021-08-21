d39929683c34dc9e647fc72e2ea40d6687051aa4
hossein-heydari <hosseinheydari790228@gmail.com>
2021-08-21 22:00:32 +0430

    add photo and product

---
+++ src/Components/PhotoViewer/PhotoViewr.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react'
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/swiper.min.css";
+import "swiper/components/navigation/navigation.min.css"
+import "swiper/components/thumbs/thumbs.min.css"
+import SwiperCore, {
+    Navigation, Thumbs
+} from 'swiper/core';
+SwiperCore.use([Navigation, Thumbs]);
+const PhotoViewr = (props) => {
+    const [thumbsSwiper, setThumbsSwiper] = useState(null);
+    return (
+        <div className="w-100">
+            <Swiper
+                style={{ width: "100%" }}
+                spaceBetween={10}
+                navigation={true}
+                thumbs={{ swiper: thumbsSwiper }}
+                className="mySwiper mb-2 w-100 h-100">
+                <SwiperSlide><img className="w-100 h-100 px-5" src={props.photo} /></SwiperSlide>
+            </Swiper>
+            <Swiper
+                style={{ width: "100%", height: "100px" }}
+                onSwiper={setThumbsSwiper}
+                spaceBetween={5}
+                slidesPerView={3.5}
+                freeMode={true}
+                watchSlidesVisibility={true}
+                watchSlidesProgress={true}
+                className="mySwiper">
+                <SwiperSlide><img style={{ border: "1px solid #4c5e60", borderRadius: "3px" }} className="w-100 h-100" src={props.photo} /></SwiperSlide>
+            </Swiper>
+        </div>
+    )
+}
+export default PhotoViewr;
---
+++ src/Components/ProductCard/ProductCard.jsx
@@ -0,0 +1,20 @@
+import './ProductCard.style.css'
+import { useDispatch } from 'react-redux';
+import React from 'react';
+const ProductCard = ({ id, image, model, category, price, btnColor = "bg-success" }d) => {
+    const dispatch = useDispatch()
+    const handleAddToCart = (event) => {
+        event.stopPropagation()
+        dispatch({ type: "addProduct", payload: { id, model, category, price, image } })
+    }
+    return (
+        <div className="product-card-container w-100" >
+            <div className="text-center"><img className="" src={image} alt="" /></div>
+            <div className="text-center"><h4>{model}</h4></div>
+            <div className="text-center">{category}</div>
+            <div className="text-center" style={{ color: "#3Bc9a7" }}>{price}<span style={{ color: "#3Bc9a7" }}>تومان</span></div>
+            <div className="text-center"><button onClick={handleAddToCart} className={`w-75 text-white btn ${"btn" + btnColor.slice(2)}`}>خرید</button></div>
+        </div>
+    )
+}
+export default ProductCard;
