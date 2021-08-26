37cbf5a49e45934587243a3615c17bea11bc00c9
hossein-heydari <hosseinheydari790228@gmail.com>
2021-08-26 15:45:05 +0430

    add app details

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
-import './App.css';
-import { Suspense } from 'react';
 import { BrowserRouter as Router, Switch } from 'react-router-dom'
+import './App.css'
+import MyNavbar from './Components/Navbar/Navbar';
+import routes from './Routes/mainRoutes'
+import { Suspense } from 'react';
+import Fotoer from './Components/Footer/Footer'
 import { Spinner } from 'react-bootstrap';
+import PrivateRotue from './Routes/PrivateRotue';
 
 function App() {
+  document.title = "دیجی کالا"
   return (
-    <>hossein</>
+    <Router>
+      <div className="app bg-light">
+        <MyNavbar /><Spinner animation="border" variant="primary" />
+        <Suspense fallback={<></>}>
+          <Switch>
+            {
+              routes.map(({ path, exact, Component, loginRequired }, index) => {
+                return (
+                  <PrivateRotue
+                    key={path + index}
+                    path={path}
+                    exact={exact}
+                    loginRequired={loginRequired}
+                    Component={Component}
+                  />
+                )
+              })
+            }
+          </Switch>
+          <Fotoer />
+        </Suspense>
+      </div>
+    </Router>
   );
 }
 
