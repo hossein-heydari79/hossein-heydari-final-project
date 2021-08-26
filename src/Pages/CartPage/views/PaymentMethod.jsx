02e9c9eafd6ed1c96118ec34025199a0be170ec9
hossein-heydari <hosseinheydari790228@gmail.com>
2021-08-26 15:44:55 +0430

    add pay page

---
+++ src/Pages/CartPage/views/PaymentMethod.jsx
@@ -0,0 +1,129 @@
+import React, { useState, useEffect } from 'react'
+import { RouteComponentProps } from 'react-router-dom'
+import { useHistory } from 'react-router'
+import { Button, Form } from 'react-bootstrap';
+import { context } from '../CartPage'
+import { useContext } from 'react';
+import { useSelector } from 'react-redux'
+import axios from 'axios';
+const PaymentMethod = (props) => {
+    const { localState, localDispatch } = useContext(context)
+    const history = useHistory();
+    const [windowWidth, setWindowWith] = useState(window.innerWidth)
+    const handleClick = () => {
+        localDispatch({ type: "payment", payload: payment })
+        axios.post("/order", {
+            ...localState, ...payment
+        })
+        history.push("/cart/verifypayment")
+    }
+    const cartItems = useSelector(state => state.cart.cartProducts)
+    useEffect(() => {
+        window.addEventListener("resize", () => {
+            setWindowWith(window.innerWidth)
+        })
+    }, [])
+    const [payment, setPayment] = useState({
+        paymentMethod: "",
+        paymentBank: ""
+    })
+    const handleChange = (event) => {
+        const data = event.target
+        setPayment({ ...payment, [data.name]: data.value })
+    }
+    return (
+        <div className="container w-100 d-flex">{console.log(payment)}
+            <div className={`bg-light mt-4 p-3 ${windowWidth < 500 ? "w-100" : "w-75"}`}>
+                <h4>روشهای پرداخت</h4>
+                <div className="row d-flex justify-content-lg-between justify-content-center">
+                    <div className="col-lg-5 bg-white p-4 mt-3"
+                        style={{ boxShadow: "0 5px 8px -3px rgb(0 0 0 / 10%)", borderRadius: "8px" }}
+                    >
+                        <h5>انتخاب نوع پرداخت</h5>
+                        <Form onChange={handleChange}>
+                            <Form.Check name="paymentMethod" value="online" type="radio" id="default-radio" label="آنلاین" />
+                            <Form.Check name="paymentMethod" value="cash" type="radio" id="default-radio" label="نقدی" />
+                        </Form>
+                    </div>
+                    <div className="col-lg-6 bg-white p-4 mt-3"
+                        style={{ boxShadow: "0 5px 8px -3px rgb(0 0 0 / 10%)", borderRadius: "8px" }}
+                    >
+                        <h5>انتخاب درگاه پرداخت</h5>
+                        <Form onChange={handleChange}>
+                            <Form.Check name="paymentBank" value="saman" type="radio" id="default-radio" label="درگاه بانک سامان" />
+                            <Form.Check name="paymentBank" value="mellat" type="radio" id="default-radio" label="درگاه بانک ملت" />
+                        </Form>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="mt-3 bg-white w-100 p-4"
+                        style={{ boxShadow: "0 5px 8px -3px rgb(0 0 0 / 10%)", borderRadius: "8px" }}
+                    >
+                        <label htmlFor="">کد تخفیف</label>
+                        <div className="d-flex">
+                            <Form.Control
+                                className="bg-light"
+                                style={{ borderRadius: "22px", border: "none" }}
+                                type="text" placeholder="در صورت داشتن کد تخفیف آن را وارد کنید" />
+                            <Button variant="outline-success" className="mx-4">اعمال</Button>
+                        </div>
+                    </div>
+                </div>
+                {
+                    windowWidth < 500 &&
+                    <Button className="w-100"
+                        style={{
+                            position: "sticky", left: "0", bottom: "12%",
+                            backgroundColor: "#3BC9A7"
+                        }}
+                        onClick={handleClick}
+                    >
+                        پرداخت و ثبت نهایی
+                    </Button>
+                }
+            </div>
+            {
+                windowWidth > 500 &&
+                <div className="w-25 my-4 mx-1 bg-white"
+                    style={{ boxShadow: "0 5px 8px -3px rgb(0 0 0 / 10%)", borderRadius: "8px", height: "200px" }}>
+                    <div className="rounded p-3">
+                        <div>
+                            <h5>خلاصه فاکتور</h5>
+                            <hr />
+                        </div>
+                        <div className="my-3">
+                            <span>مجموع سبد خرید :  </span>
+                            <strong><span className="text-success">
+                                {
+                                    cartItems.reduce((accumulator: number, current) => {
+                                        return accumulator + (+current.price * current.count);
+                                    }, 0)
+                                }
+                            </span></strong>
+                        </div>
+                        <div className="my-3">
+                            <span>مجموع کل :  </span>
+                            <strong>
+                                <span className="text-success">
+                                    {
+                                        cartItems.reduce((accumulator: number, current) => {
+                                            return accumulator + (+current.price * current.count);
+                                        }, 0)
+                                    }
+                                </span>
+                            </strong>
+                        </div>
+                        <div className="text-center">
+                            <button
+                                style={{ fontSize: "small" }}
+                                onClick={handleClick}
+                                className="btn btn-primary w-100">پرداخت و ثبت نهایی
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            }
+        </div>
+    )
+}
+export default PaymentMethod;
---
+++ src/Pages/CartPage/views/VerifyAdress.jsx
@@ -0,0 +1,184 @@
+import React, { useState, useEffect } from 'react'
+import { Button, Form } from 'react-bootstrap'
+import { RouteComponentProps } from 'react-router-dom'
+import { useHistory } from 'react-router'
+import { useSelector } from 'react-redux'
+import Proviences from '../../../Assets/provinces.json'
+import Cities from '../../../Assets/cities.json'
+import { context } from '../CartPage'
+import { useContext } from 'react';
+const VerifyAdress = (props) => {
+    const { localState, localDispatch } = useContext(context)
+    const cartItems = useSelector(state => state.cart.cartProducts)
+    const history = useHistory();
+    const [windowWidth, setWindowWith] = useState(window.innerWidth)
+    const [cities, setCities] = useState([Cities[0]])
+    const [userInfo, setUserInfo] = useState({
+        postalCode: "",
+        fullName: "",
+        provience: "",
+        city: "",
+        address: "",
+    })
+    const [postMethod, setPostMethod] = useState("")
+    const userInput = (event) => {
+        const data = event.target;
+        // console.log(data.value)
+        setUserInfo({ ...userInfo, [data.name]: data.value })
+    }
+    useEffect(() => {
+        window.addEventListener("resize", () => {
+            setWindowWith(window.innerWidth)
+        })
+    }, [])
+    const handleProvienceChange = (event) => {
+        const data = event.target
+        setCities(Cities.filter(item => +item.province_id === +data.value))
+        const provience = Proviences.find(item => item.id === +data.value)
+        provience && setUserInfo({ ...userInfo, provience: provience.name })
+    }
+    const handleCityChange = (event) => {
+        const data = event.target
+        setUserInfo({ ...userInfo, city: data.value })
+    }
+    const handlePostMethod = (event) => {
+        const data = event.target
+        setPostMethod(data.value)
+    }
+    const handleClick = () => {
+        localDispatch({ type: "addUserInfo", payload: { userInfo, postMethod } })
+        history.push("/cart/payment")
+    }
+    return (
+        <div className="container w-100 d-flex">
+            <div className={`bg-light mt-4 p-3 ${windowWidth < 500 ? "w-100" : "w-75"}`} >{console.log(userInfo)}
+                <div className="container w-100">
+                    <div className="row d-flex justify-content-lg-between justify-content-center">
+                        <div className="col-lg-9 col-12 row bg-white p-3"
+                            style={{ boxShadow: "0 5px 8px -3px rgb(0 0 0 / 10%)", borderRadius: "8px" }}
+                        >
+                            <h4 className="mb-3">نشانی</h4>
+                            <div className="col-lg-6 col-12">
+                                <label className="my-2" htmlFor="">کدپستی</label>
+                                <Form.Control className="bg-light" style={{ borderRadius: "22px", border: "none" }}
+                                    type="text" placeholder="کدپستی خود را وارد کنید"
+                                    name="postalCode"
+                                    onChange={userInput} />
+                            </div>
+                            <div className="col-lg-6 col-12">
+                                <label className="my-2" htmlFor="">نام تحویل گیرنده</label>
+                                <Form.Control className="bg-light"
+                                    style={{ borderRadius: "22px", border: "none" }}
+                                    type="text" placeholder="نام گیرنده را وارد کنید"
+                                    name="fullName"
+                                    onChange={userInput} />
+                            </div>
+                            <div className="col-lg-6 col-12">
+                                <label className="my-2" htmlFor="">استان</label>
+                                <Form.Select className="bg-light"
+                                    style={{ borderRadius: "22px", border: "none", fontSize: "smaller" }}
+                                    onChange={handleProvienceChange} aria-label="Default select example">
+                                    <option>یک مورد را انتخاب نمایید</option>
+                                    {
+                                        Proviences.map((provience) => (
+                                            <option key={provience.id + provience.name} value={provience.id}>{provience.name}</option>
+                                        ))
+                                    }
+                                </Form.Select>
+                            </div>
+                            <div className="col-lg-6 col-12">
+                                <label className="my-2" htmlFor="">شهر</label>
+                                <Form.Select className="bg-light"
+                                    style={{ borderRadius: "22px", border: "none", fontSize: "smaller" }}
+                                    aria-label="Default select example"
+                                    onChange={handleCityChange}
+                                >
+                                    <option>یک مورد را انتخاب نمایید</option>
+                                    {
+                                        cities.map((city) => (
+                                            <option key={city.id + city.name} value={city.name}>{city.name}</option>
+                                        ))
+                                    }
+                                </Form.Select>
+                            </div>
+                            <div className="col-12">
+                                <label className="my-2" htmlFor="">آدرس دقیق</label>
+                                <Form.Control className="bg-light"
+                                    style={{ borderRadius: "22px", border: "none" }}
+                                    as="textarea" placeholder="آدرس دقیق محل تحویل کالا را وارد نمایید"
+                                    name="address"
+                                    onChange={userInput}
+                                />
+                            </div>
+                        </div>
+                        {/* <div className="col-lg-3 col-12 h-100 bg-white mt-lg-0 mt-3 p-4"
+                            style={{ boxShadow: "0 5px 8px -3px rgb(0 0 0 / 10%)", borderRadius: "8px" }}
+                        >
+                            <h4 className="mb-3">روش ارسال</h4>
+                            <Form onChange={handlePostMethod}>
+                                <Form.Check value="pishtaz" name="postOption" type="radio" id="default-radio" label="پست پیشتاز" />
+                                <Form.Check value="tipax" name="postOption" type="radio" id="default-radio" label="تیپاکس" />
+                                <Form.Check value="barbari" name="postOption" type="radio" id="default-radio" label="باربری" />
+                                <Form.Check value="bus" name="postOption" type="radio" id="default-radio" label="اتوبوس" />
+                            </Form>
+                        </div> */}
+                    </div>
+                </div>
+                {
+                    windowWidth < 500 &&
+                    <Button className="w-100"
+                        style={{
+                            position: "sticky", left: "0", bottom: "12%",
+                            backgroundColor: "#3BC9A7"
+                        }}
+                        onClick={handleClick}
+                    >
+                        ادامه و انتخاب روش پرداخت
+                    </Button>
+                }
+            </div>
+            {
+                windowWidth > 500 &&
+                <div className="w-25 my-4 mx-1 bg-white"
+                    style={{ boxShadow: "0 5px 8px -3px rgb(0 0 0 / 10%)", borderRadius: "8px", height: "200px" }}>
+                    <div className="rounded p-3">
+                        <div>
+                            <h5>خلاصه فاکتور</h5>
+                            <hr />
+                        </div>
+                        <div className="my-3">
+                            <span>مجموع سبد خرید :  </span>
+                            <strong><span className="text-success">
+                                {
+                                    cartItems.reduce((accumulator: number, current) => {
+                                        return accumulator + (+current.price * current.count);
+                                    }, 0)
+                                }
+                            </span></strong>
+                        </div>
+                        <div className="my-3">
+                            <span>مجموع کل :  </span>
+                            <strong>
+                                <span className="text-success">
+                                    {
+                                        cartItems.reduce((accumulator: number, current) => {
+                                            return accumulator + (+current.price * current.count);
+                                        }, 0)
+                                    }
+                                </span>
+                            </strong>
+                        </div>
+                        <div className="text-center">
+                            <button
+                                style={{ fontSize: "small" }}
+                                onClick={handleClick}
+                                className="btn btn-primary w-100">ادامه و انتخاب روش پرداخت
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            }
+        </div>
+    )
+}
+export default VerifyAdress;
