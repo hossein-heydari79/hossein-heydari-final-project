310a431c50a1c09ff5ee49f1fd7d4a3c6283fc08
hossein-heydari <hosseinheydari790228@gmail.com>
2021-08-26 15:44:21 +0430

    add login

---
+++ src/Pages/Login/Login.jsx
@@ -0,0 +1,108 @@
+import React, { useState } from 'react'
+import axios from 'axios';
+import { Button, Form } from 'react-bootstrap';
+import { RouteComponentProps } from 'react-router'
+import { useDispatch } from 'react-redux'
+import { MdLockOpen } from "react-icons/md";
+import { useHistory } from 'react-router-dom';
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+export const Login = (props) => {
+    const dispatch = useDispatch()
+    const history = useHistory()
+    const [user, setUser] = useState({ email: "", password: "" })
+    const handleChange = (event) => {
+        const data = event.target
+        setUser({ ...user, [data.name]: data.value })
+    }
+    const handleLogin = (event) => {
+        event.preventDefault()
+        axios.post('/login', {
+            email: user.email,
+            password: user.password
+        })
+            .then((res) => {
+                console.log(res.data)
+                dispatch({
+                    type: "userLogin", payload: {
+                        isLogin: true,
+                        accessToken: res.data.accessToken,
+                        refreshToken: res.data.refreshToken,
+                        role: "",
+                        firstName: res.data.firstName,
+                        lastName: res.data.lastName,
+                        phoneNumber: res.data.phoneNumber
+                    }
+                })
+                toast.success('با موفقیت وارد شدید', {
+                    position: "bottom-center",
+                    autoClose: 2000,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true,
+                    progress: undefined,
+                });
+                setTimeout(() => history.push("/homepage"), 2000)
+            })
+            .catch(err => {
+                toast.error('کاربر وجود ندارد', {
+                    position: "bottom-center",
+                    autoClose: 2000,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true,
+                    progress: undefined,
+                });
+                console.log(err)
+            })
+    }
+    return (
+        <div className="w-100 bg-light h-100" style={{ marginTop: "6rem" }}>
+            <div className="container w-100 h-75 d-flex justify-content-center align-items-center">
+                <Form className="bg-white p-5 rounded"
+                    style={{ boxShadow: "0 5px 8px -3px rgb(0 0 0 / 10%)", borderRadius: "8px" }}
+                >
+                    <div className="text-center">
+                        <MdLockOpen
+                            className="p-3"
+                            style={{ width: "100px", height: "100px", backgroundColor: "#fff9f9", color: "#e05c5c", borderRadius: "50%" }} />
+                    </div>
+                    <div className="text-center"><h1>Login</h1></div>
+                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                        <Form.Label>ایمیل</Form.Label>
+                        <Form.Control
+                            style={{ borderRadius: "22px", border: "none", backgroundColor: "#e8f0fe" }}
+                            onChange={handleChange} name="email" type="email" placeholder="آدرس ایمیل" />
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="formBasicPassword">
+                        <Form.Label>رمز عبور</Form.Label>
+                        <Form.Control
+                            style={{ borderRadius: "22px", border: "none", backgroundColor: "#e8f0fe" }}
+                            onChange={handleChange} name="password" type="password" placeholder="کلمه عبور" />
+                    </Form.Group>
+                    <Button
+                        style={{ backgroundColor: "#3bc9a7" }}
+                        className="w-100 mt-3"
+                        onClick={handleLogin} variant="primary" type="submit">
+                        ورود به حساب
+                    </Button>
+                </Form>
+            </div>
+            <ToastContainer
+                position="bottom-center"
+                autoClose={5000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={true}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+            />
+        </div>
+    )
+}
+export default Login;
---
+++ src/Pages/Register/Register.jsx
@@ -0,0 +1,119 @@
+import axios from 'axios';
+import React, { useState, useEffect } from 'react'
+import { Button, Form } from 'react-bootstrap';
+import { RouteComponentProps } from 'react-router'
+import { useHistory } from 'react-router'
+import { MdPersonAdd } from "react-icons/md";
+
+
+export const Register = (props) => {
+    const history = useHistory()
+    const [user, setUser] = useState({ firstName: "", lastName: "", email: "", password: "", phoneNumber: "" })
+    const [message, setMessage] = useState("")
+    const handleChange = (event) => {
+        const data = event.target
+        setUser({ ...user, [data.name]: data.value })
+    }
+    const handleRegister = (event) => {
+        event.preventDefault()
+        axios.post('/register', {
+            ...user
+        })
+            .then((res) => {
+                if (res.data === "user already exists") {
+                    setMessage("کاربر وجود دارد.لطفا وارد شوید.")
+                    setTimeout(() => {
+                        history.push("/login")
+                    }, 1000)
+                }
+                else if (res.data === "successed") {
+                    setMessage("شما به طور اتوماتیک به صفحه ورود هدایت میشوید.")
+                    setTimeout(() => {
+                        history.push("/login")
+                    }, 2000)
+                }
+                console.log(res.data)
+            })
+            .catch(err => {
+                console.log(err.message)
+                setMessage(err.message)
+            })
+    }
+    const [windowWidth, setWindowWith] = useState(window.innerWidth)
+    useEffect(() => {
+        window.addEventListener("resize", () => {
+            setWindowWith(window.innerWidth)
+        })
+    }, [])
+    return (
+        <div className="w-100 mb-5 bg-light" style={{ marginTop: `${windowWidth < 992 ? "3rem" : "6rem"}` }}>
+            <div className={`container d-flex justify-content-center align-items-center
+            ${windowWidth < 992 ? "w-100" : "w-50"}
+            `}>
+                <Form className="bg-white p-5 rounded"
+                    style={{ boxShadow: "0 5px 8px -3px rgb(0 0 0 / 10%)", borderRadius: "8px" }}>
+                    <div className="text-center">
+                        <MdPersonAdd
+                            className="p-3"
+                            style={{ width: "100px", height: "100px", backgroundColor: "#f3fff9", color: "#3cc9a7", borderRadius: "50%" }} />
+                    </div>
+                    <div className="text-center"><h5>حساب کاربری جدید بسازید</h5></div>
+                    <div className="row">
+                        <div className="col-lg-6 col-12 mt-2">
+                            <Form.Label>نام</Form.Label>
+                            <Form.Control
+                                className="bg-light"
+                                style={{ borderRadius: "22px", border: "none" }}
+                                onChange={handleChange} name="firstName" type="text" placeholder="نام خود را وارد نمایید" />
+                        </div>
+                        <div className="col-lg-6 col-12 mt-2">
+                            <Form.Label>ایمیل</Form.Label>
+                            <Form.Control
+                                className="bg-light"
+                                style={{ borderRadius: "22px", border: "none" }}
+                                onChange={handleChange} name="email" type="email" placeholder="آدرس ایمیل خود را وارد نمایید" />
+                        </div>
+                        <div className="col-lg-6 col-12 mt-2">
+                            <Form.Label>نام خانوادگی</Form.Label>
+                            <Form.Control
+                                className="bg-light"
+                                style={{ borderRadius: "22px", border: "none" }}
+                                onChange={handleChange} name="lastName" type="text" placeholder="نام خانوادگی خود را وارد نمایید" />
+                        </div>
+                        <div className="col-lg-6 col-12 mt-2">
+                            <Form.Label>شماره موبایل</Form.Label>
+                            <Form.Control
+                                className="bg-light"
+                                style={{ borderRadius: "22px", border: "none" }}
+                                onChange={handleChange} name="phoneNumber" type="email" placeholder="شماره موبایل خود را وارد نمایید" />
+                        </div>
+                        <div className="col-lg-6 col-12 mt-2">
+                            <Form.Label>کلمه عبور</Form.Label>
+                            <Form.Control
+                                className="bg-light"
+                                style={{ borderRadius: "22px", border: "none" }}
+                                onChange={handleChange} name="password" type="password" placeholder="کلمه عبور خود را وارد نمایید" />
+                        </div>
+                        <div className="col-lg-6 col-12 mt-2">
+                            <Form.Label>تایید کلمه عبور</Form.Label>
+                            <Form.Control
+                                className="bg-light"
+                                style={{ borderRadius: "22px", border: "none" }}
+                                onChange={handleChange} name="password" type="password" placeholder="کلمه عبور را مجددا وارد نمایید" />
+                        </div>
+                    </div>
+                    <small>{message}</small>
+                    <div className="w-100 px-5">
+                        <Button
+                            style={{ backgroundColor: "#3bc9a7" }}
+                            className="w-100 mt-3"
+                            onClick={handleRegister} variant="primary" type="submit">
+                            ایجاد حساب کاربری
+                        </Button>
+                    </div>
+                </Form>
+            </div>
+        </div>
+    )
+}
+export default Register;
