201ceb8058b3ee5a89d9cd8e30b3158f53f41fa7
hossein-heydari <hosseinheydari790228@gmail.com>
2021-08-27 15:12:38 +0430

    fix notfound

--- src/Pages/404Page/NotFound.jsx
+++ src/Pages/404Page/NotFound.jsx
@@ -8,7 +8,7 @@ const NotFound = () => {
             <section className="page_404">
                 <div className="container">
                     <div className="row">
-                        <div className="col-sm-12 ">
+                        <div className="col-sm-12 d-flex flex-column align-items-center">
                             <div className="col-sm-10 col-sm-offset-1  text-center">
                                 <div className="four_zero_four_bg">
                                     <h1 className="text-center ">404</h1>
