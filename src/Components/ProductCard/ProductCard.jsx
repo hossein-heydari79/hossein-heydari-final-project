529e576ef15eabd169b19aaa8d5fbf1eab523fef
hossein-heydari <hosseinheydari790228@gmail.com>
2021-08-28 18:59:13 +0430

    fix btn card

--- src/Components/ProductCard/ProductCard.jsx
+++ src/Components/ProductCard/ProductCard.jsx
@@ -1,6 +1,8 @@
 import './ProductCard.style.css'
 import { useDispatch } from 'react-redux';
 import React from 'react';
+import { FaShoppingCart, FaHeart } from "react-icons/fa";
+
 const ProductCard = ({ id, image, model, category, price, btnColor = "bg-success" }) => {
     const dispatch = useDispatch()
     const handleAddToCart = (event) => {
@@ -12,7 +14,12 @@ const ProductCard = ({ id, image, model, category, price, btnColor = "bg-success
             <div className="text-center"><img className="m-4" src={image} alt="" /></div>
             <div className="text-center m-2"><h5>{model}</h5></div>
             <div className="text-center m-2">{price}<span style={{ margin: "10px" }}>تومان</span></div>
-            <div className="text-center m-2"><button onClick={handleAddToCart} className={`w-100 text-white btn btn-primary`}>خرید</button></div>
+            <div className="text-center m-2">
+                <div className="w-100 d-flex justify-content-between align-items-center px-3 py-1">
+                    <FaShoppingCart size="20px" className="text-success sell-icons" onClick={handleAddToCart} style={{ cursor: "pointer" }} />
+                    <FaHeart size="20px" className="text-danger sell-icons" onClick={(e) => e.stopPropagation()} style={{ cursor: "pointer" }} />
+                </div>
+            </div>
         </div>
     )
 }
